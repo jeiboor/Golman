1e242b01f8a5799b73ac39fd1c888c71da2bac44
Dr0virus <e.nourifard110@gmail.com>
2016-03-17 08:24:55 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {139178069, 162637521},
     admin_users = {},
     disabled_channels = {}
   }
